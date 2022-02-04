70286debc01a13acf00b9c5122ede8f15aff0fca
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2022-02-04 21:33:20 +0100

    Beautify code

--- src/app/stat-list/stat-list.component.ts
+++ src/app/stat-list/stat-list.component.ts
@@ -30,15 +30,15 @@ export class StatListComponent implements OnInit {
   calculateCupsDrank(currentLocalHour: number): void {
     switch(true) {
       // From 8:00 to 10:59
-      case (currentLocalHour >= 8 && currentLocalHour <= 10): // TODO: any cleaner way to write this?
+      case (this.isValueInRange(currentLocalHour, 8, 10)):
         this.numberOfCups = 1;
         break;
       // From 11:00 to 15:59
-      case (currentLocalHour >= 11 && currentLocalHour <= 15):
+      case (this.isValueInRange(currentLocalHour, 11, 15)):
         this.numberOfCups = 2;
         break;
       // From 16:00 to 23:59
-      case (currentLocalHour >= 16 && currentLocalHour <= 23):
+      case (this.isValueInRange(currentLocalHour, 16, 23)):
         this.numberOfCups = 3;
         break;
       // Any other time (covers cases from 0:00 to 7:59)
@@ -47,7 +47,11 @@ export class StatListComponent implements OnInit {
     }
   }
 
-  toggleUnitTestList() {
+  toggleUnitTestList(): void {
     this.isUnitTestListVisible = !this.isUnitTestListVisible;
+  }
+
+  private isValueInRange(value: number, min: number, max: number): boolean {
+    return value >= min && value <= max;
   }
 }
