04b21d401bad49bc70985c1890c90ed775809f81
Ziga Vukcevic <ziga.vukcevic@gmail.com>
2022-04-11 15:35:37 +0200

    Add logic for meter

--- src/app/roadmap/roadmap-item/roadmap-item.component.ts
+++ src/app/roadmap/roadmap-item/roadmap-item.component.ts
@@ -16,13 +16,69 @@ export class RoadmapItemComponent implements OnInit {
   @Input() version?: RoadmapItemInterface['version'];
   @Input() child?: RoadmapItemInterface['child'] | null;
 
-  constructor() {}
+  itemListCount: { all: number, visible: number };
+
+  constructor() {
+    this.itemListCount = { all: 0, visible: 0 };
+  }
 
   ngOnInit(): void {}
 
-  toggleChild() {
+  toggleChildVisibility() {
     if (this.child) {
       this.child.isVisible = !this.child.isVisible;
+      this.calculateDepth();
     }
+  }
+
+  countAllItems(itemList: any) {
+    console.log('countAllItems');
+
+    itemList.forEach((item: any) => {
+      // Counts
+      this.itemListCount.all = this.itemListCount.all + 1;
+
+      // Go deeper
+      item.child && this.countAllItems(item.child.itemList);
+    });
+
+    return this.itemListCount.all;
+  }
+
+  countVisibleItems(child: any) {
+    console.log('countVisibleItems');
+
+    if (child && child.itemList && child.isVisible) {
+      child.itemList.forEach((item: any) => {
+        // Counts
+        this.itemListCount.visible = this.itemListCount.visible + 1;
+
+        if (item.child && item.child.isVisible) {
+          // Go deeper
+          this.itemListCount.visible = this.itemListCount.visible + item.child.itemList.length;
+          this.countVisibleItems(item.child.itemList);
+        }
+      });
+    }
+
+    return this.itemListCount.visible;
+  }
+
+  calculateDepth(): string {
+    if (this.child) { // && this.child?.itemList.length === 8
+      this.itemListCount = { all: 0, visible: 0 };
+      if (this.child) {
+        this.countAllItems(this.child.itemList);
+        this.countVisibleItems(this.child);
+        // FIXME: still something not working, it gets stuck at visible 13 (no matter if all are open)
+        console.log(this.itemListCount);
+      }
+    }
+
+    const percentage = this.itemListCount.all
+      ? Math.round(this.itemListCount.visible / this.itemListCount.all * 100)
+      : 0;
+
+    return `${percentage}%`;
   }
 }
